56863749cfbc7e56dc417a0077ffb7091a6b5d77
Мария Фалилеева <falileeva.ma@gmail.com>
2024-12-12 16:14:35 +0800

    fix event preview container

--- frontend/src/components/EventPreviewContainer/EventPreviewContainer.tsx
+++ frontend/src/components/EventPreviewContainer/EventPreviewContainer.tsx
@@ -15,23 +15,26 @@ export default function EventPreviewContainer(
     const [activeIndex, setActiveIndex] = useState(null)
 
     return (
-        <div
-            className={styles.root}
-            onMouseOver={() => setActive(true)}
-            onMouseOut={() => {
-                setActive(false)
-                setActiveIndex(null)
-            }}
-        >
-            {eventList.map((item, index) => (
-                <EventPreview
-                    key={item.id}
-                    event={item}
-                    isActiveContainer={isActive}
-                    onMouseOver={() => setActiveIndex(index)}
-                    isActive={activeIndex === index}
-                />
-            ))}
+        <div className={styles.wrap}>
+            <div
+                className={styles.list}
+                onMouseOver={() => setActive(true)}
+                onMouseOut={() => {
+                    setActive(false)
+                    setActiveIndex(null)
+                }}
+            >
+                {eventList.map((item, index) => (
+                    <EventPreview
+                        key={item.id}
+                        event={item}
+                        isActiveContainer={isActive}
+                        onMouseOver={() => setActiveIndex(index)}
+                        isActive={activeIndex === index}
+                    />
+                ))}
+            </div>
         </div>
+
     );
 }
