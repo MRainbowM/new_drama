cd6f9dce025506590b76139460a509a0023f8d35
Мария Фалилеева <falileeva.ma@gmail.com>
2024-12-16 18:01:03 +0800

    arrow

--- frontend/src/components/EventPreviewSection/EventPreviewSection.tsx
+++ frontend/src/components/EventPreviewSection/EventPreviewSection.tsx
@@ -1,6 +1,7 @@
 import styles from './EventPreviewSection.module.scss'
 import { apiClient } from '../../api/client'
 import EventPreviewList from '../EventPreviewContainer/EventPreviewContainer';
+import Arrow from 'public/static/images/arrow.svg'
 
 export default async function EventPreviewSection() {
     // Репертуар
@@ -20,9 +21,24 @@ export default async function EventPreviewSection() {
     return (
         <section className={styles.root}>
             <h2>Все спектакли</h2>
-            <EventPreviewList
-                eventList={response.data}
-            />
+            <div className={styles.container}>
+                <div className={styles.arrowLeft}>
+                    <div className={styles.arrow}>
+                        <Arrow />
+                    </div>
+                </div>
+
+                <EventPreviewList
+                    eventList={response.data}
+                />
+
+                <div className={styles.arrowRight}>
+                    <div className={styles.arrow}>
+                        <Arrow />
+                    </div>
+                </div>
+            </div>
+
         </section>
     )
 }
