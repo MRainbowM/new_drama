f289ab04ef8e792721fc9686453868cd0b3ec017
Мария Фалилеева <falileeva.ma@gmail.com>
2024-12-19 17:24:15 +0800

    create ScheduleSection

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,12 @@
 import EventPreviewSection from '../components/EventPreviewSection/EventPreviewSection';
 import Hero from '../components/Hero/Hero';
+import ScheduleSection from '../components/ScheduleSection/ScheduleSection';
 import '../styles/page.scss'
 
 export default async function MainPage() {
-
-
     return (<>
         <Hero />
+        <ScheduleSection />
         <EventPreviewSection />
     </>);
 }
---
+++ frontend/src/components/ScheduleSection/ScheduleSection.tsx
@@ -0,0 +1,11 @@
+import styles from './ScheduleSection.module.scss'
+
+export default function ScheduleSection() {
+    return (
+        <section className={styles.root}>
+            <h2>Афиша</h2>
+
+            
+        </section>
+    );
+}
