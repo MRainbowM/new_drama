fa55b86d0348fc1dd8b3fa2cbd11f45aee67ac3c
Мария Фалилеева <falileeva.ma@gmail.com>
2024-12-19 17:06:46 +0800

    Hero block

--- frontend/src/components/Hero/Hero.tsx
+++ frontend/src/components/Hero/Hero.tsx
@@ -1,19 +1,26 @@
 import styles from './Hero.module.scss'
-import TitleNew from 'public/static/images/hero_1.svg'
-import TitleDrama from 'public/static/images/hero_2.svg'
+import NDlarge from 'public/static/images/hero_2.svg?url'
+import NDsmall from 'public/static/images/hero_1.svg?url'
+
 
 export default function Hero() {
     return (
         <div className={styles.root}>
-            <div className={styles.mainVideo}>
-                <video autoPlay muted loop preload="auto" aria-label="Video player">
+            <div className={styles.mainVideo} >
+                <video
+                    className={styles.bgVideo}
+                    autoPlay
+                    muted
+                    loop
+                    preload="auto"
+                    aria-label="Video player"
+                    style={{
+                        '--mask-large': `url(${NDlarge.src})`,
+                        '--mask-small': `url(${NDsmall.src})`,
+                    } as React.CSSProperties}
+                >
                     <source src='http://0.0.0.0:8011/static/video/hero.MP4' type="video/mp4" />
                 </video>
-
-            </div>
-            <div className={styles.title}>
-                <TitleNew style={{ marginRight: '-50%' }} />
-                <TitleDrama style={{ marginLeft: '-50%' }} />
             </div>
         </div>
     )
