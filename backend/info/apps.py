9cdb941124041feddd4fab5e6dc2926da4e068dd
Мария Фалилеева <falileeva.ma@gmail.com>
2024-12-25 12:46:41 +0800

    add info app

---
+++ backend/info/apps.py
@@ -0,0 +1,10 @@
+from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
+
+
+class InfoConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'info'
+    verbose_name = _('Инфо-блоки')
+
+
