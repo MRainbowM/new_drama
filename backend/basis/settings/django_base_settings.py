9989281db679bd41bb224457bb1d6bfa5c3c5bd5
Мария Фалилеева <falileeva.ma@gmail.com>
2025-01-17 10:10:54 +0800

    fix MEDIA_URL

--- backend/basis/settings/django_base_settings.py
+++ backend/basis/settings/django_base_settings.py
@@ -114,7 +114,7 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_URL = '/media/'
+MEDIA_URL = env('MEDIA_URL')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
