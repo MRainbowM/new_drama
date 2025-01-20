67e0fe44f1f0d85e90cfcda99a2484fb3e3be25f
Мария Фалилеева <falileeva.ma@gmail.com>
2025-01-20 15:01:32 +0800

    fix Hero video

--- frontend/src/components/Hero/Hero.tsx
+++ frontend/src/components/Hero/Hero.tsx
@@ -1,11 +1,10 @@
 import styles from './Hero.module.scss'
 import NDlarge from 'public/images/hero_2.svg?url'
 import NDsmall from 'public/images/hero_1.svg?url'
-// import videoHero from 'public/video/hero.mp4'
+
 
 export default function Hero() {
-    const videoPath = `${process.env.BACKEND_STATIC_URL}/video/hero.MP4`;
-
+    const videoPath = `${process.env.BACKEND_STATIC_URL}/video/hero.mp4`;
 
     return (
         <div className={styles.root}>
@@ -22,9 +21,8 @@ export default function Hero() {
                         '--mask-small': `url(${NDsmall.src})`,
                     } as React.CSSProperties}
                 >
+
                     <source src={videoPath} type="video/mp4" />
-
-
                 </video>
             </div>
         </div>
