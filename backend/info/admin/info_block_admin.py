b05ec92e31368bb05e6fc46bf2637a6b5dd1de53
Мария Фалилеева <falileeva.ma@gmail.com>
2025-01-23 14:32:26 +0800

    fix admin

--- backend/info/admin/info_block_admin.py
+++ backend/info/admin/info_block_admin.py
@@ -5,4 +5,5 @@ from ..models import InfoBlock
 
 @admin.register(InfoBlock)
 class InfoBlockAdmin(admin.ModelAdmin):
+    list_display = ('title', 'is_enable', 'sort', 'create_at', 'update_at')
     readonly_fields = ('create_at', 'update_at')
