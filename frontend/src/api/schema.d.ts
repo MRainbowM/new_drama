0ca81fd15081f11de970bd60eb596efae17d1a9e
Мария Фалилеева <falileeva.ma@gmail.com>
2025-01-23 11:57:13 +0800

    fix schema

--- frontend/src/api/schema.d.ts
+++ frontend/src/api/schema.d.ts
@@ -123,6 +123,23 @@ export interface paths {
         patch?: never;
         trace?: never;
     };
+    "/info/viewer/list": {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
+        };
+        /** Получить список зрителей */
+        get: operations["info_api_get_viewer_list"];
+        put?: never;
+        post?: never;
+        delete?: never;
+        options?: never;
+        head?: never;
+        patch?: never;
+        trace?: never;
+    };
 }
 export type webhooks = Record<string, never>;
 export interface components {
@@ -363,6 +380,21 @@ export interface components {
              */
             cover: string;
         };
+        /** ViewerFilterSchema */
+        ViewerFilterSchema: {
+            /** Is Enable */
+            is_enable?: boolean | null;
+        };
+        /** ViewerOutSchema */
+        ViewerOutSchema: {
+            /** ID */
+            id?: number | null;
+            /**
+             * Фотография
+             * @description Изображение на главной
+             */
+            image: string;
+        };
     };
     responses: never;
     parameters: never;
@@ -524,6 +556,28 @@ export interface operations {
                 };
                 content: {
                     "application/json": components["schemas"]["InfoBlockOutSchema"][];
+                };
+            };
+        };
+    };
+    info_api_get_viewer_list: {
+        parameters: {
+            query?: {
+                is_enable?: boolean | null;
+            };
+            header?: never;
+            path?: never;
+            cookie?: never;
+        };
+        requestBody?: never;
+        responses: {
+            /** @description OK */
+            200: {
+                headers: {
+                    [name: string]: unknown;
+                };
+                content: {
+                    "application/json": components["schemas"]["ViewerOutSchema"][];
                 };
             };
         };
