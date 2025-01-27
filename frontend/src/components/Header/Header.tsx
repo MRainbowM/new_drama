d7b74e2401b5c823243cc89399910f0bc5175240
Мария Фалилеева <falileeva.ma@gmail.com>
2025-01-27 17:05:20 +0800

    fix header

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -3,33 +3,37 @@ import Logo from 'public/images/nd.svg'
 import Link from 'next/link'
 
 export default function Header() {
+    const menuItems = [
+        { href: '/#schedule', title: 'Афиша' },
+        { href: '/#events', title: 'Спектакли' },
+        { href: '/#contacts', title: 'Контакты' },
+        { href: '/', title: 'Ресторан' },
+        { href: '/', title: 'Аренда' },
+        { href: '/#partner', title: 'Партнеры' },
+    ];
+    const middleIdx = Math.floor(menuItems.length / 2);
+    const leftItems = menuItems.slice(0, middleIdx);
+    const rightItems = menuItems.slice(middleIdx, menuItems.length);
+
     return (
         <header className={styles.root}>
             <div className={styles.menu}>
                 <div className={styles.leftCol}>
-                    <Link href='/#schedule'>
-                        <span>Афиша</span>
-                    </Link>
-                    <Link href='/#events'>
-                        <span>Спектакли</span>
-                    </Link>
-                    <Link href='/#contacts'>
-                        <span>Контакты</span>
-                    </Link>
+                    {leftItems.map(item => (
+                        <Link href={item.href}>
+                            <span>{item.title}</span>
+                        </Link>
+                    ))}
                 </div>
                 <div className={styles.logo}>
                     <Link href='/'><Logo /></Link>
                 </div>
                 <div className={styles.rightCol}>
-                    <Link href='/'>
-                        <span>Ресторан</span>
-                    </Link>
-                    <Link href='/'>
-                        <span>Аренда</span>
-                    </Link>
-                    <Link href='/#partner'>
-                        <span>Партнеры</span>
-                    </Link>
+                    {rightItems.map(item => (
+                        <Link href={item.href}>
+                            <span>{item.title}</span>
+                        </Link>
+                    ))}
                 </div>
             </div>
         </header>
