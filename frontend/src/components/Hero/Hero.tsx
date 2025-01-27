f092c9dabf5248e421225bb94cd181c2f3933bad
Мария Фалилеева <falileeva.ma@gmail.com>
2025-01-27 14:48:46 +0800

    fix Hero mobile

--- frontend/src/components/Hero/Hero.tsx
+++ frontend/src/components/Hero/Hero.tsx
@@ -1,6 +1,6 @@
 import styles from './Hero.module.scss'
 import NDlarge from 'public/images/hero_2.svg?url'
-import NDsmall from 'public/images/hero_1.svg?url'
+import NDsmall from 'public/images/hero_m.svg?url'
 
 
 export default function Hero() {
