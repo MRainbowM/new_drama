ba1a7b80cac987832ad3e357fce6fdac895035b4
Мария Фалилеева <falileeva.ma@gmail.com>
2025-01-27 13:20:23 +0800

    add Header

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import Container from '../components/Container/Container'
 import Footer from '../components/Footer/Footer'
+import Header from '../components/Header/Header'
 import '../styles/globals.scss'
 import { sans_narrow, montserrat } from './fonts'
 import clsx from "clsx"
@@ -18,6 +19,7 @@ export default function RootLayout({
                 )}
             >
                 <Container>
+                    <Header />
                     {children}
                     <Footer />
                 </Container>
