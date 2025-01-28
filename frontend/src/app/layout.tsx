a59eea8bd41b834621958049d9e759424f1cfcdc
Мария Фалилеева <falileeva.ma@gmail.com>
2025-01-28 14:05:11 +0800

    fix header

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -20,8 +20,8 @@ export default function RootLayout({
                 )}
             >
                 <RunningLine />
+                <Header />
                 <Container>       
-                    <Header />
                     {children}
                     <Footer />
                 </Container>
