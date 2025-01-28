9702194c8e6c0de73959f63cdad8df9636f4f3ba
Мария Фалилеева <falileeva.ma@gmail.com>
2025-01-28 13:40:44 +0800

    get menu data

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,16 +1,40 @@
 import styles from './Header.module.scss'
 import Logo from 'public/images/nd.svg'
 import Link from 'next/link'
+import { apiClient } from '../../api/client'
 
-export default function Header() {
+export default async function Header() {
+    // Инфо-блоки в меню
+    const response = await apiClient.GET('/info/info_block/menu/list', {
+        params: {
+            query: {
+                is_enable: true,
+                in_menu: true
+            }
+        }
+    });
+
+    if (response.error) {
+        console.log(response.error);
+        throw new Error('error'); //TODO
+    }
+
     const menuItems = [
         { href: '/#schedule', title: 'Афиша' },
         { href: '/#events', title: 'Спектакли' },
         { href: '/#contacts', title: 'Контакты' },
-        { href: '/', title: 'Ресторан' },
-        { href: '/', title: 'Аренда' },
         { href: '/#partner', title: 'Партнеры' },
     ];
+
+    if (response.data.length > 0) {
+        response.data.map(item => (
+            menuItems.push({
+                href: `/#${item.menu_title_slug}`,
+                title: item.menu_title
+            })
+        ));
+    }
+
     const middleIdx = Math.floor(menuItems.length / 2);
     const leftItems = menuItems.slice(0, middleIdx);
     const rightItems = menuItems.slice(middleIdx, menuItems.length);
