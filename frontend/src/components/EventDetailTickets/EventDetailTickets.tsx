1c609a3bf7c7eb47a04014affd51d4c6463cefae
Мария Фалилеева <falileeva.ma@gmail.com>
2025-02-07 19:06:20 +0800

    not found text

--- frontend/src/components/EventDetailTickets/EventDetailTickets.tsx
+++ frontend/src/components/EventDetailTickets/EventDetailTickets.tsx
@@ -10,21 +10,26 @@ interface EventDetailTicketsProps {
 export default async function EventDetailTickets(
     { event_id }: EventDetailTicketsProps
 ) {
-    const { events, months } = await getEventShowList({event_id});
+    const { events, months } = await getEventShowList({ event_id });
 
     return (
-        <>
+        <section className={styles.root}>
+            <h2>Билеты</h2>
             {events.length > 0 ? (
-                <section className={styles.root}>
-                    <h2>Билеты</h2>
-                    <ScheduleList
-                        events={events}
-                        months={months}
-                    />
-                </section>
+                <ScheduleList
+                    events={events}
+                    months={months}
+                />
             ) : (
-                <></>
+                <div className={styles.notFoundText}>
+                    <span>В этом месяце не ставим, следите за афишей, чтобы не пропустить </span>
+
+                    <div  className={styles.social}>
+
+                    </div>
+                </div>
             )}
-        </>
+        </section>
+
     );
 }
