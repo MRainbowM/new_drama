38b23d1e9754ab9aa196a879527faeddd481af59
Мария Фалилеева <falileeva.ma@gmail.com>
2025-02-07 18:47:58 +0800

    init getEventShowList

--- frontend/src/components/EventDetailTickets/EventDetailTickets.tsx
+++ frontend/src/components/EventDetailTickets/EventDetailTickets.tsx
@@ -1,6 +1,6 @@
-import styles from './EventDetailTickets.module.scss'
-import { apiClient } from '../../api/client'
+import styles from './EventDetailTickets.module.scss';
 import ScheduleList from '../ScheduleList/ScheduleList';
+import { getEventShowList } from '../../services/getEventShowList';
 
 interface EventDetailTicketsProps {
     event_id: number
@@ -10,41 +10,15 @@ interface EventDetailTicketsProps {
 export default async function EventDetailTickets(
     { event_id }: EventDetailTicketsProps
 ) {
-    // Спектакли в афише
-    const response = await apiClient.GET('/event/event_show/list', {
-        params: {
-            query: {
-                is_enable: true,
-                from_current_month: true,
-                event_id: event_id
-            }
-        }
-    });
-
-    if (response.error) {
-        console.log(response.error);
-        throw new Error('error'); //TODO
-        return (<></>);
-    }
-
-    let months = [] // Месяцы со спектаклями
-    response.data.forEach(function (event, i, arr) {
-        const startDate = new Date(event.start_at)
-        const month = startDate.getMonth().toString()
-
-        if (!months.includes(month)) {
-            months.push(month)
-        }
-    });
+    const { events, months } = await getEventShowList({event_id});
 
     return (
         <>
-            {response.data.length > 0 ? (
-                <section className={styles.root} id="schedule">
-                    <h2>Афиша</h2>
-
+            {events.length > 0 ? (
+                <section className={styles.root}>
+                    <h2>Билеты</h2>
                     <ScheduleList
-                        events={response.data}
+                        events={events}
                         months={months}
                     />
                 </section>
