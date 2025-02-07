c979b41d2d8bebbfcd30b171eaf4aab78c69e1fe
Мария Фалилеева <falileeva.ma@gmail.com>
2025-02-07 18:49:45 +0800

    rf

--- frontend/src/components/ScheduleSection/ScheduleSection.tsx
+++ frontend/src/components/ScheduleSection/ScheduleSection.tsx
@@ -1,43 +1,19 @@
 import styles from './ScheduleSection.module.scss'
-import { apiClient } from '../../api/client'
 import ScheduleList from '../ScheduleList/ScheduleList';
+import { getEventShowList } from '../../services/getEventShowList';
 
 
 export default async function ScheduleSection() {
-    // Спектакли в афише
-    const response = await apiClient.GET('/event/event_show/list', {
-        params: {
-            query: {
-                is_enable: true,
-                from_current_month: true
-            }
-        }
-    });
-
-    if (response.error) {
-        console.log(response.error);
-        throw new Error('error'); //TODO
-        return (<></>);
-    }
-
-    let months = [] // Месяцы со спектаклями
-    response.data.forEach(function (event, i, arr) {
-        const startDate = new Date(event.start_at)
-        const month = startDate.getMonth().toString()
-
-        if (!months.includes(month)) {
-            months.push(month)
-        }
-    });
+    const { events, months } = await getEventShowList();
 
     return (
         <>
-            {response.data.length > 0 ? (
+            {events.length > 0 ? (
                 <section className={styles.root} id="schedule">
                     <h2>Афиша</h2>
 
                     <ScheduleList
-                        events={response.data}
+                        events={events}
                         months={months}
                     />
                 </section>
