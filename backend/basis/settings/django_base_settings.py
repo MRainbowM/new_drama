4cf204af94826f6970f117fd9a6e9f5bd5b6d18e
Мария Фалилеева <falileeva.ma@gmail.com>
2025-02-10 17:00:14 +0800

    connect grappelli

--- backend/basis/settings/django_base_settings.py
+++ backend/basis/settings/django_base_settings.py
@@ -21,6 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # Application definition
 
 INSTALLED_APPS = [
+    'grappelli',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
