d1a9d84bac63e29a905cb8b736ac1c3deafc122f
Мария Фалилеева <falileeva.ma@gmail.com>
2025-02-17 17:12:08 +0800

    theater mobile

--- frontend/src/components/TheaterSection/TheaterSection.tsx
+++ frontend/src/components/TheaterSection/TheaterSection.tsx
@@ -1,10 +1,32 @@
+'use client'
 import styles from './TheaterSection.module.scss'
 import TheaterSVG from 'public/images/theater.svg'
+import TheaterMobileSVG from 'public/images/theater_mobile.svg'
+import { useCallback, useEffect, useRef, useState } from 'react'
 
 export default function TheaterSection() {
+    // Брейкпоинт мобильной версии
+    const mobileWidth = 700;
+
+    const [isMobile, setMobile] = useState(false);
+
+    useEffect(() => {
+        if(window.innerWidth <=mobileWidth) {
+            setMobile(true);
+        } else {
+            setMobile(false);
+        }    
+    }, []);
+
     return (
         <div className={styles.root}>
-            <TheaterSVG />
+            {
+                isMobile ? (
+                    <TheaterMobileSVG />
+                ) : (
+                    <TheaterSVG />
+                )
+            }
         </div>
     );
 }
