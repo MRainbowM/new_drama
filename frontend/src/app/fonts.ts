848202b58da98df43d30bda2fe8b4e89167e1b31
Мария Фалилеева <falileeva.ma@gmail.com>
2025-02-18 10:54:07 +0800

    add PT_Sans_Caption font

--- frontend/src/app/fonts.ts
+++ frontend/src/app/fonts.ts
@@ -1,16 +1,22 @@
-import { PT_Sans_Narrow, Montserrat } from 'next/font/google'
+import { PT_Sans_Narrow, Montserrat, PT_Sans_Caption } from 'next/font/google'
 
 export const sans_narrow = PT_Sans_Narrow({
     weight: ['400', '700'],
     subsets: ['latin', 'cyrillic'],
 
-     variable: '--font-sans-narrow'
+    variable: '--font-sans-narrow'
 })
 
 export const montserrat = Montserrat({
     weight: ['500'],
     subsets: ['latin', 'cyrillic'],
-    
 
     variable: '--font-montserrat'
+})
+
+export const sans_caption = PT_Sans_Caption({
+    weight: ['400'],
+    subsets: ['latin', 'cyrillic'],
+
+    variable: '--font-sans-caption'
 })
--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,7 +2,7 @@ import Container from '../components/Container/Container'
 import Footer from '../components/Footer/Footer'
 import Header from '../components/Header/Header'
 import '../styles/globals.scss'
-import { sans_narrow, montserrat } from './fonts'
+import { sans_narrow, montserrat, sans_caption } from './fonts'
 import clsx from "clsx"
 import { getMenuItems } from '../services/getMenuItems';
 
@@ -23,7 +23,8 @@ export default async function RootLayout({
                 className={clsx(
                     sans_narrow.className,
                     `${montserrat.variable} font-sans`,
-                    `${sans_narrow.variable} font-sans`
+                    `${sans_narrow.variable} font-sans`,
+                    `${sans_caption.variable} font-sans`,
                 )}
             >
                 {runline}
