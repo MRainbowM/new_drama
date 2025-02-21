f16f7fe905f7d0e2b0e41dff66ff76f2699607f7
Мария Фалилеева <falileeva.ma@gmail.com>
2025-02-21 12:56:49 +0800

    rename field

--- backend/event/admin/event_show_admin.py
+++ backend/event/admin/event_show_admin.py
@@ -5,6 +5,10 @@ from ..models import EventShow
 
 @admin.register(EventShow)
 class EventShowAdmin(admin.ModelAdmin):
-    list_display = ('event__name', 'start_at')
-    sortable_by = ('event__name', 'start_at')
+    list_display = ('get_event_name', 'start_at',)
+    sortable_by = ('start_at', 'get_event_name')
     ordering = ('-start_at',)
+
+    @admin.display(description='Спектакль', ordering='event__name')
+    def get_event_name(self, obj) -> str:
+        return obj.event.name
