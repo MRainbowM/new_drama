5563f313fe7124fefce40fb2159de23ccdf60a98
Мария Фалилеева <falileeva.ma@gmail.com>
2025-02-26 12:55:21 +0800

    fix ym

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -7,10 +7,9 @@ import clsx from "clsx"
 import { getMenuItems } from '../services/getMenuItems';
 import RunningLine from '../components/RunningLine/RunningLine'
 import Noise from '../libs/Noise/Noise'
-import Script from 'next/script';
-import YandexMetrika from '../components/YandexMetrika/YandexMetrika'
-import { Suspense } from 'react';
+import { Metrika } from '../components/YandexMetrika/YandexMetrika'
 
+import { Suspense } from "react";
 export const dynamic = 'force-dynamic';
 
 export default async function RootLayout({
@@ -36,30 +35,9 @@ export default async function RootLayout({
 
                 {
                     process.env.YM_COUNTER ? (
-                        <>
-                            <Script id="metrika-counter" strategy="afterInteractive">
-                                {
-                                    `(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-                                m[i].l=1*new Date();
-                                for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-                                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-                                (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-                            
-                                ym(${process.env.YM_COUNTER}, "init", {
-                                    defer: true,
-                                    clickmap:true,
-                                    trackLinks:true,
-                                    accurateTrackBounce:true,
-                                    webvisor:true
-                                }
-                            );`
-                                }
-                            </Script>
-
-                            <Suspense fallback={<></>}>
-                                <YandexMetrika />
-                            </Suspense>
-                        </>
+                        <Suspense >
+                            <Metrika />
+                        </Suspense>
                     ) : (<></>)
                 }
 
---
+++ frontend/src/components/YandexMetrika/YandexMetrika.js
@@ -0,0 +1,36 @@
+"use client";
+
+import { useEffect } from "react";
+import { usePathname, useSearchParams } from "next/navigation";
+import Script from "next/script";
+
+export function Metrika() {
+  const pathName = usePathname();
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    ym(process.env.YM_COUNTER, "hit", window.location.href);
+  }, [pathName, searchParams]);
+
+  return (
+    <Script id="yandex-metrika">
+      {`
+        (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();
+        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+        ym(${process.env.YM_COUNTER}, "init", {
+          defer: true,
+          clickmap:true,
+          trackLinks:true,
+          accurateTrackBounce:true
+        });    
+      `}
+    </Script>
+  );
+}
+
+
+
