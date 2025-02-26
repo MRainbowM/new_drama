e8eb5fcfb8142e4cfbfc1e272d89c3106ccc756b
Мария Фалилеева <falileeva.ma@gmail.com>
2025-02-26 12:28:13 +0800

    YandexMetrika

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -7,6 +7,9 @@ import clsx from "clsx"
 import { getMenuItems } from '../services/getMenuItems';
 import RunningLine from '../components/RunningLine/RunningLine'
 import Noise from '../libs/Noise/Noise'
+import Script from 'next/script';
+import YandexMetrika from '../components/YandexMetrika/YandexMetrika'
+import { Suspense } from 'react';
 
 export const dynamic = 'force-dynamic';
 
@@ -30,6 +33,36 @@ export default async function RootLayout({
 
                 style={{ marginTop: -21 }}
             >
+
+                {
+                    process.env.YM_COUNTER ? (
+                        <>
+                            <Script id="metrika-counter" strategy="afterInteractive">
+                                {
+                                    `(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                                m[i].l=1*new Date();
+                                for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+                                (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+                            
+                                ym(${process.env.YM_COUNTER}, "init", {
+                                    defer: true,
+                                    clickmap:true,
+                                    trackLinks:true,
+                                    accurateTrackBounce:true,
+                                    webvisor:true
+                                }
+                            );`
+                                }
+                            </Script>
+
+                            <Suspense fallback={<></>}>
+                                <YandexMetrika />
+                            </Suspense>
+                        </>
+                    ) : (<></>)
+                }
+
                 <Noise
                     patternSize={250}
                     patternScaleX={1}
