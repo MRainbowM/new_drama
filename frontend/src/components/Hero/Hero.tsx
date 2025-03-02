cc441b17986bbb6f59fe0f57de3d4ce3a5984c93
Мария Фалилеева <falileeva.ma@gmail.com>
2025-03-02 21:21:23 +0800

    del controls

--- frontend/src/components/Hero/Hero.tsx
+++ frontend/src/components/Hero/Hero.tsx
@@ -24,8 +24,6 @@ export default function Hero() {
                         '--mask-large': `url(${NDlarge.src})`,
                         '--mask-small': `url(${NDsmall.src})`,
                     } as React.CSSProperties}
-
-                    controls
                 >
                     <source src="/api/video?is_mobile=true" type="video/mp4" media="(max-width:700px)"  />
                     <source src="/api/video?is_mobile=false" type="video/mp4"  />
