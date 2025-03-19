b18a0fb3b61578face6c77c30ecf4c91028f4ffb
Мария Фалилеева <falileeva.ma@gmail.com>
2025-03-19 18:23:08 +0800

    add urls_namespace

--- backend/basis/api.py
+++ backend/basis/api.py
@@ -4,7 +4,7 @@ from event.api import router as event_router
 from info.api import router as info_router
 from people.api import router as people_router
 
-api = NinjaAPI()
+api = NinjaAPI(urls_namespace='api')
 
 api.add_router("/event/", event_router)
 api.add_router("/people/", people_router)
