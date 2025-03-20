8e9b485865b7bdede3332790a23430e39fbc819f
Мария Фалилеева <falileeva.ma@gmail.com>
2025-03-20 18:56:10 +0800

    close popup

--- frontend/src/components/PopupModal/PopupModal.tsx
+++ frontend/src/components/PopupModal/PopupModal.tsx
@@ -2,20 +2,51 @@
 import { components } from '../../api/schema';
 import styles from './PopupModal.module.scss';
 import Link from 'next/link';
+import { useCallback, useEffect, useRef, useState } from 'react';
+import clsx from 'clsx';
 
 interface PopupModalProps {
     popup: components['schemas']['PopupOutSchema']
 }
 
-export default async function PopupModal(
+export default function PopupModal(
     { popup }: PopupModalProps
 ) {
-    return (
-        <div className={styles.root}>
+    const [isClose, setClose] = useState(false);
 
-            <div className={styles.modal}>
+    const onClickClose = () => {
+        setClose(true);
+    }
+
+    // При клике вне модалки - закрывать ее
+    const refModal = useRef<HTMLDivElement>();
+
+    useEffect(() => {
+        function handleClick(event) {
+            if (refModal.current && !refModal.current.contains(event.target)) {
+                onClickClose();
+            }
+        }
+        document.addEventListener('mousedown', handleClick);
+        return () => {
+            document.removeEventListener('mousedown', handleClick);
+        };
+    }, [onClickClose]);
+
+    return (
+        <div
+            className={clsx(
+                styles.root,
+                { [styles.close]: isClose }
+            )}
+        >
+
+            <div className={styles.modal} ref={refModal}>
                 <div className={styles.topRow}>
-                    <div className={styles.btnClose}>
+                    <div
+                        className={styles.btnClose}
+                        onClick={onClickClose}
+                    >
                         <div className={styles.lineTop}></div>
                         <div className={styles.lineBot}></div>
                     </div>
@@ -32,7 +63,7 @@ export default async function PopupModal(
                     >
                     </div>
                     <div className={styles.btnAction}>
-                        <Link href={popup.btn_link} >
+                        <Link href={popup.btn_link} target="__blank">
                             <span>{popup.btn_text}</span>
                         </Link>
                     </div>
