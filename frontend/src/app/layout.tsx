65f58588d42adb15a5e8698785fd2be7f0da8da9
Мария Фалилеева <falileeva.ma@gmail.com>
2025-04-09 21:28:17 +0800

    fix lang

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -23,7 +23,7 @@ export default async function RootLayout({
     const { menuItems } = await getMenuItems();
 
     return (
-        <html lang="en">
+        <html lang="ru">
             <head>
                 {
                     YM_COUNTER ? (
