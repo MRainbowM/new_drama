146475e1759192fa8a6e5bc2f60a17cd0f2fec79
Мария Фалилеева <falileeva.ma@gmail.com>
2026-02-11 18:40:20 +0800

    popup

--- frontend/src/components/Popup/Popup.tsx
+++ frontend/src/components/Popup/Popup.tsx
@@ -1,15 +1,12 @@
-import styles from './Popup.module.scss';
-
-import { apiClient } from '../../api/client';
 import PopupModal from '../PopupModal/PopupModal';
+import { getPopupActive } from '../../services/api/getPopupActive';
 
 export default async function Popup() {
-    const response = await apiClient.GET('/info/popup', { cache: 'no-cache' });
-
-    if (response.error) return null;
-
-
+    
+    const { popup } = await getPopupActive();
+    if (!popup) return null;
+    
     return (
-        <PopupModal popup={response.data} />
+        <PopupModal popup={popup} />
     );
 }
